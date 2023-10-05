0ea1b87df053bad9f388746715e6b7e3c7aea40c
Juan David Patarroyo Sánchez <juandavid.patarroyosanchez@gmail.com>
2023-10-05 08:35:56 -0500

    Commit final

--- JFramePractice/src/ModelView/JFramePractice.java
+++ JFramePractice/src/ModelView/JFramePractice.java
@@ -14,40 +14,6 @@ public class JFramePractice{
         //RadioButtonDemo.MostrarDemo(root);
         //ComboBoxDemo.CBDemostracion(root);
         ListDemo.MostrarLista(root);
-        
-        //Configuación del marco
-        
-//        JFrame ventanita = new JFrame("Nueva ventana") ;   
-//        
-//        ventanita.setVisible(true);     
-//        ventanita.setDefaultCloseOperation(3);
-//        ventanita.setBounds(100, 100, 500, 500);
-//
-//           
-//        //Panel principal
-//        JPanel panel = new JPanel();
-//        panel.setBackground(Color.red);
-//        //panel.setLayout(null);
-//        ventanita.add(panel);
-//        
-//        //Boton
-//        JButton boton = new JButton("Boton");
-//        boton.setBounds(50, 50, 200, 100);
-//        panel.add(boton);
-//        
-//        //Etiqueta
-//        JLabel etiqueta = new JLabel("Etiqueta");
-//        etiqueta.setBounds(50, 50, 20, 20);
-//        panel.add(etiqueta);
-        
-        
-        
-        
-        
-        
-        
-        
-        // TODO code application logic here
     }
     public static JFrame CreacionFrame(){
         JFrame root = new JFrame("");
